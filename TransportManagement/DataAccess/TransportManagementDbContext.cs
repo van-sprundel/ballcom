3cfcbe220a99f0a1ffeac907db71d4d8eb4a64f9
Lauran van Ekeren <lauranvanekeren@gmail.com>
2022-05-17 18:29:24 +0200

    transportmanagement database added

---
+++ TransportManagement/DataAccess/TransportManagementDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TransportManagement.Models;
+
+namespace TransportManagement.DataAccess;
+
+public class TransportManagementDbContext : DbContext
+{
+    public TransportManagementDbContext(DbContextOptions<TransportManagementDbContext> options) : base(options)
+    {
+        
+    }
+    
+    public DbSet<TransportCompany> TransportCompanies { get; set; }
+    public DbSet<Order> Orders { get; set; }
+
+}
