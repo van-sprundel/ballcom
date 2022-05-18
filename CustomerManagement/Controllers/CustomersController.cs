30687e8f5c5cee81cae954f97a5e0362f9f45741
Lauran van Ekeren <lauranvanekeren@gmail.com>
2022-05-18 15:04:16 +0200

    begin routing customermanagement. Des te meer men zich ergert aan errors, des te blijer men raakt wanneer het opgelost is.

---
+++ CustomerManagement/Controllers/CustomersController.cs
@@ -0,0 +1,72 @@
+﻿using CustomerManagement.DataAccess;
+using CustomerManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CustomerManagement.Controllers;
+
+[Route("/api/[controller]")]
+public class CustomersController : Controller
+{
+    CustomerManagementDbContext _dbContext;
+
+    public CustomersController(CustomerManagementDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    [HttpGet]
+    [Route("test", Name = "Test")]
+    public async Task<IActionResult> Test()
+    {
+        return Ok("test");
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllAsync()
+    {
+        Console.WriteLine("called");
+        return Ok(await _dbContext.Customers.ToListAsync());
+    }
+
+    [HttpGet]
+    [Route("{customerId}", Name = "GetByCustomerId")]
+    public async Task<IActionResult> GetByCustomerId(int customerId)
+    {
+        var customer = await _dbContext.Customers.FirstOrDefaultAsync(c => c.CustomerId == customerId);
+        if (customer == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(customer);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> RegisterAsync([FromBody] Customer customer)
+    {
+        try
+        {
+            if (ModelState.IsValid)
+            {
+                // insert customer
+                _dbContext.Customers.Add(customer);
+                await _dbContext.SaveChangesAsync();
+
+                //TODO: sent event
+
+                // return result
+                return CreatedAtRoute("GetByCustomerId", new { customerId = customer.CustomerId }, customer);
+            }
+
+            return BadRequest();
+        }
+        catch (DbUpdateException)
+        {
+            ModelState.AddModelError("", "Unable to save changes. ");
+            return StatusCode(StatusCodes.Status500InternalServerError);
+            throw;
+        }
+    }
+
+}
--- CustomerManagement/Program.cs
+++ CustomerManagement/Program.cs
@@ -1,6 +1,7 @@
 using CustomerManagement.DataAccess;
 using Microsoft.EntityFrameworkCore;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
 // add DBContext
@@ -9,11 +10,27 @@ builder.Services.AddDbContext<CustomerManagementDbContext>(options =>
     options.UseMySql(mariaDbConnectionString, ServerVersion.AutoDetect(mariaDbConnectionString)));
 
 
+// Add framework services
+builder.Services
+    .AddMvc(options => options.EnableEndpointRouting = false);
+
+// setup MVC
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World from customermanagement!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
 
+app.UseMvc();
+app.UseDefaultFiles();
+app.UseStaticFiles();
 
+app.MapControllers();
+
+app.MapGet("/", () => "Hello World from customermanagement!!!!!");
 
 Console.WriteLine("Starting application");
 app.Run();
