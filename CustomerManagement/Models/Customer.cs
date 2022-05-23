71e88bc75c78f321432b2ef684fc97907690f3a2
juliusvanleeuwen <58423129+juliusvanleeuwen@users.noreply.github.com>
2022-05-23 16:52:53 +0200

    CustomersController
    Use of viewmodels added

--- CustomerManagement/Models/Customer.cs
+++ CustomerManagement/Models/Customer.cs
@@ -1,12 +1,13 @@
-﻿namespace CustomerManagement.Models;
-
-public class Customer
+﻿namespace CustomerManagement.Models
 {
-    public int CustomerId { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string Email { get; set; }
-    public string Address { get; set; }
-    public string City { get; set; }
-    //TODO
+
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public string? Address { get; set; }
+        public string? City { get; set; }
+    }
 }
