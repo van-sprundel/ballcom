4854242f707031a45547e810751100e8c036022e
Martijn Laffort <martijn.laffort@hotmail.com>
2022-05-25 18:32:36 +0200

    Made orderpicker logic

--- Orderpicker/Controllers/OrderpickerController.cs
+++ Orderpicker/Controllers/OrderpickerController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Orderpicker.DataAccess;
 using Orderpicker.Models;
+using BallCore.Enums;
 
 namespace Orderpicker.Controllers;
 
@@ -30,21 +31,61 @@ public class OrderpickerController : Controller
         {
             return NotFound();
         }
-
+        order.OrderProducts = _dbContext.OrderProducts.Where(op => op.OrderId == orderId).ToList();
         return Ok(order);
     }
 
-    [HttpGet]
-    [Route("{productId}", Name = "GetByProductId")]
-    public async Task<IActionResult> GetByProductId(int productId)
+    [HttpPost]
+    [Route("{orderNumber}/{productNumber}", Name = "AddProductToOrder")]
+    public async Task<IActionResult> AddProductToOrderAsync(int orderNumber, int productNumber)
     {
-        var product = await _dbContext.Products.FirstOrDefaultAsync(p => p.ProductId == productId);
-        if (product == null)
+        try
         {
-            return NotFound();
-        }
+            int amountProducts = _dbContext.OrderProducts.Count(op => op.OrderId == orderNumber);
+            if (amountProducts < 20)
+            {
+                var order = await _dbContext.Orders.FirstOrDefaultAsync(o => o.OrderId == orderNumber);
+                var product = await _dbContext.Products.FirstOrDefaultAsync(p => p.ProductId == productNumber);
+                if (product != null && order != null)
+                {
+                    if (order.OrderStatus != StatusProcess.Pending)
+                    {
+                        return StatusCode(StatusCodes.Status403Forbidden, "Order has already been submitted. No changes allowed.");
+                    }
+                    if (product.Quantity < 1)
+                    {
+                        return StatusCode(StatusCodes.Status410Gone, "Order is out of stock");
+                    }
+                    OrderProduct orderProduct = new OrderProduct
+                    {
+                        OrderId = orderNumber,
+                        ProductId = productNumber
+                    };
+                    // Insert
+                    _dbContext.OrderProducts.Add(orderProduct);
+                    _dbContext.Orders.Update(order);
+                    await _dbContext.SaveChangesAsync();
 
-        return Ok(product);
+                    // return result
+                    return Ok();
+                }
+                else
+                {
+                    return StatusCode(StatusCodes.Status404NotFound, "ProductNumber or OrderNumber could not be found.");
+                }
+            }
+            else
+            {
+                return StatusCode(StatusCodes.Status412PreconditionFailed, "This order already contains 20 products, more are not allowed.");
+
+            }
+
+        }
+        catch (DbUpdateException)
+        {
+            ModelState.AddModelError("", "Unable to save changes.");
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
     }
 
 }
--- Orderpicker/Models/Order.cs
+++ Orderpicker/Models/Order.cs
@@ -7,6 +7,7 @@ public class Order
 {
     public int OrderId { get; set; }
     public StatusProcess OrderStatus { get; set; }
+    public bool IsDone { get; set; }
+    public ICollection<OrderProduct> OrderProducts { get; set; }
 
-    public List<Product> Products { get; set; }
 }
