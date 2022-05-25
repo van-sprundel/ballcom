da03fb9ef29b443fe8458496a3a942fa9fea4878
Martijn Laffort <martijn.laffort@hotmail.com>
2022-05-25 16:49:44 +0200

    Added OrderProduct

---
+++ Orderpicker/Models/OrderProduct.cs
@@ -0,0 +1,11 @@
+namespace Orderpicker.Models;
+
+public class OrderProduct
+{
+    public int OrderProductId { get; set; }
+    public int OrderId { get; set; }
+    public Order Order { get; set; }
+
+    public int ProductId { get; set; }
+    public Product Product { get; set; }
+}
