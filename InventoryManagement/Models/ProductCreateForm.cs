6d1f6fcec4f768948eaeed25bda9c47b55bd6762
Martijn Laffort <martijn.laffort@hotmail.com>
2022-05-26 14:40:48 +0200

    Can't create a product anymore in inventory

--- InventoryManagement/Models/ProductCreateForm.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace InventoryManagement.Models
-{
-    public class ProductCreateForm
-    {
-        public string Name { get; set; }
-        public int Quantity { get; set; }
-        public double Price { get; set; }
-    }
-}
