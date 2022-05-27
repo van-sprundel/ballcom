edd9a391aac4f140a399be24aafbec7b22671afd
Martijn Laffort <martijn.laffort@hotmail.com>
2022-05-27 09:38:46 +0200

    Changed models Order and OrderProduct

--- Orderpicker/Models/Order.cs
+++ Orderpicker/Models/Order.cs
@@ -6,6 +6,5 @@ public class Order
 {
     public int Id { get; set; }
     public StatusProcess Status { get; set; }
-    public bool IsDone { get; set; }
     public ICollection<OrderProduct> Products { get; set; }
 }
